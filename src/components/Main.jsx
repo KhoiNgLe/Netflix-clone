0d1ebc85e109cbc14dc60a2886365d44f568f256
KhoiNgLe <nguy0572@algonquinlive.com>
2022-09-14 21:17:06 -0400

    Finish Navbar & Add API

---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import requests from '../Requests';
+import axios from 'axios'
+
+const Main = () => {
+  const [movies, setMovies] = useState([]);
+
+const movie = movies[Math.floor(Math.random() * movies.length)]
+
+
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
+  console.log(movie);
+  return <div></div>;
+};
+
+export default Main;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="">
+    <div className="flex items-center justify-between p-4 z-10 absolute w-full">
       <h1 className="text-red-600 text-4xl font-bold cursor-pointer">Netflix</h1>
       <div>
-        <button>Sign In</button>
-        <button className="bg-red-600">Sign Up</button>
+        <button className="text-white pr-4">Sign In</button>
+        <button className="bg-red-600 px-6 py-4 rounded cursor-pointer text-white">Sign Up</button>
       </div>
     </div>
   );
