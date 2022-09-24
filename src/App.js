91c2bae44b694ca7c55e0a7c574fd5eee52732ca
KhoiNgLe <nguy0572@algonquinlive.com>
2022-09-23 21:24:56 -0400

    Add pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthContextProvider } from "./context/AuthContext";
 import Account from "./pages/Account";
 import Home from "./pages/Home";
@@ -15,10 +16,10 @@ function App() {
       </AuthContextProvider>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/signup' element={<Signup />}></Route>
-        <Route path='/account' element={<Account />}></Route>
+        <Route path='/' element={<ProtectedRoute><Home /></ProtectedRoute>}/>
+        <Route path='/login' element={<ProtectedRoute><Login /></ProtectedRoute>}></Route>
+        <Route path='/signup' element={<ProtectedRoute><Signup /></ProtectedRoute>}></Route>
+        <Route path='/account' element={<ProtectedRoute><Account /></ProtectedRoute>}></Route>
       </Routes>
     </div>
   );
